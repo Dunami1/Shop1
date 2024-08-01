0c3a72af97cc740f58ab2d80117b21b092c57c6d
Dima <dimamakushev2016@gmail.com>
2024-08-01 11:54:32 +0300

    Update app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,35 +2,20 @@ package app;
 
 public class Main {
 
-    static String product1;
-    static String product2;
-    static double totalsales1;
-    static double totalsales2;
-    static double dailys1;
-    static double dailys2;
-    static int days1;
-    static int days2;
-
-
+    static String[] products;
+    static double[] price;
+    static int[] days;
 
     public static void main(String[] args) {
 
-        product1 = "smartpone";
-        product2 = "laptop";
-        totalsales1 = 12153.41;
-        totalsales2 = 10486.85;
-        days1 = 5;
-        days2 = 7;
-        dailys1 = totalsales1 / days1;
-        dailys2 = totalsales2 / days2;
+        products = new String[]{"smartphone", "laptop"};
+        price = new double[]{12153.41, 10486.85};
+        days = new int[]{5, 7};
 
-        System.out.printf("Product No 1: %s%n", product1);
-        System.out.printf("Total sales for %d days is EUR %.2f%n", days1, totalsales1);
-        System.out.printf("Sales by day is EUR %.2f%n", dailys1);
-
-        System.out.printf("%nProduct No 2: %s%n", product2);
-        System.out.printf("Total sales for %d days is EUR %.2f%n", days2, totalsales2);
-        System.out.printf("Sales by day is EUR %.2f%n", dailys2);
+        for (int i = 0; i < products.length && i < price.length; i++) {
+            double salesByDay = price[i] / days[i];
+            System.out.printf("Product No %d: %s,\ntotal sales for %d days is EUR %.2f,\nsales by day is EUR %.2f.%n",
+                    i + 1, products[i], days[i], price[i], salesByDay);
+        }
     }
 }
-
